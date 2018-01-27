ba023eb990e0cb9267d856d8e1b747e28b18fa78
Gabriel Paiva Rega <gabrielrega@gmail.com>
2018-01-26 22:09:21 -0200

    First commit
    Basic function operational.

---
+++ R/read_xlsb.R
@@ -0,0 +1,18 @@
+#' Loads a xlsb file
+#' This is the basic function of the package, wich reads the xlsb files as a database using RODBC.
+#'
+#' @param x A filename or filepath.
+#' @return A dataframe containing data stored in the file.
+#' @examples
+#' DT <- read_xlsb("data/march.xlsb")
+#' DT <- read_xlsb("march.xlsb")
+
+read_xlsb <- function(x) {
+  wb <- x
+  con <- RODBC::odbcConnectExcel2007(wb)
+  tables <- RODBC::sqlTables(con)
+  sheet <- tables$TABLE_NAME[1]
+  data <- RODBC::sqlFetch(con, sheet, as.is = c(TRUE,TRUE,TRUE,TRUE))
+  RODBC::odbcCloseAll()
+  return (data)
+}
